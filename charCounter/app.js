f8ab05bdae3d4706a4e98a3126d32f04cc0be550
ruthinunes <ruthinunesdev@gmail.com>
2023-01-12 13:37:07 -0300

    finish js file

---
+++ charCounter/app.js
@@ -0,0 +1,14 @@
+const textArea = document.querySelector('#textArea');
+
+function upadateCounter() {
+    let maxChar = textArea.maxLength;
+    let totalCounter = document.querySelector('.total_counter');
+    let remainingCounter = document.querySelector('.remainig_counter');
+
+    totalCounter.innerText = textArea.value.length;
+    remainingCounter.innerText = maxChar - textArea.value.length;
+}
+
+textArea.addEventListener('keyup', function () {
+    upadateCounter();
+});
