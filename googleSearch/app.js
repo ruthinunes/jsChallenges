87771c7fe66961a4040372733ac36b91cdb72136
ruthinunes <ruthinunesdev@gmail.com>
2023-02-22 23:18:37 -0300

    Realocating clear input in a function and call it when page content is loaded

--- googleSearch/app.js
+++ googleSearch/app.js
@@ -3,13 +3,18 @@ const closeInpuCtn = document.querySelector('.input-clear');
 const iconMic = document.querySelector('.input-img-camera');
 const iconCam = document.querySelector('#serchInput');
 
-inputElement.addEventListener('keydown', function () {
-    closeInpuCtn.classList.remove('hide');
-});
+function clearInput() {
+    inputElement.addEventListener('keydown', function () {
+        closeInpuCtn.classList.remove('hide');
+    });
 
-inputElement.addEventListener('keyup', () => {
-    if (inputElement.value === '') {
-        closeInpuCtn.classList.add('hide');
-    };
-});
+    inputElement.addEventListener('keyup', () => {
+        if (inputElement.value === '') {
+            closeInpuCtn.classList.add('hide');
+        };
+    });
+};
 
+window.addEventListener('DOMContentLoaded', function () {
+    clearInput();
+});
