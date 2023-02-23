56d5991a2bf2b0765cfbaa1279458aa3a1edaa6c
ruthinunes <ruthinunesdev@gmail.com>
2023-02-23 13:54:05 -0300

    Add icons btns popups function

--- googleSearch/app.js
+++ googleSearch/app.js
@@ -1,20 +1,36 @@
-const inputElement = document.querySelector('#serchInput');
-const closeInpuCtn = document.querySelector('.input-clear');
-const iconMic = document.querySelector('.input-img-camera');
-const iconCam = document.querySelector('#serchInput');
+const inputCtn = document.querySelector('.input-search-ctn');
 
 function clearInput() {
-    inputElement.addEventListener('keydown', function () {
-        closeInpuCtn.classList.remove('hide');
-    });
+    const inputElement = document.querySelector('#serchInput');
+    const closeInputCtn = document.querySelector('.input-clear');
+    const closeInputBtn = document.querySelector('#closeInputBtn');
 
+    inputElement.addEventListener('keydown', function () {
+        closeInputCtn.classList.remove('hide');
+        showPopup('#closeInputBtn', '.popup-clear');
+    });
     inputElement.addEventListener('keyup', () => {
         if (inputElement.value === '') {
-            closeInpuCtn.classList.add('hide');
+            closeInputCtn.classList.add('hide');
         };
+    });
+    closeInputBtn.addEventListener('click', function () {
+        inputElement.value = "";
+        closeInputCtn.classList.add('hide');
+    });
+};
+
+function showPopup(element, className) {
+    inputCtn.querySelector(element).addEventListener('mouseover', function () {
+        inputCtn.querySelector(className).classList.remove('hide');
+    });
+    inputCtn.querySelector(element).addEventListener('mouseout', function () {
+        inputCtn.querySelector(className).classList.add('hide');
     });
 };
 
 window.addEventListener('DOMContentLoaded', function () {
     clearInput();
+    showPopup('.input-img-mic', '.popup-mic');
+    showPopup('.input-img-camera', '.popup-camera');
 });
