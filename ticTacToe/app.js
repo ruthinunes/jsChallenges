b720e2f814e57efe9e623032c684d836dd3d4001
ruthinunes <ruthinunesdev@gmail.com>
2023-03-17 17:00:13 -0300

    Adding reset button function / Refactoring code

--- ticTacToe/app.js
+++ ticTacToe/app.js
@@ -1,19 +1,18 @@
-const tabuleiro = document.querySelector('.quadrado-principal');
+const boardElement = document.querySelector('.board');
 const text = document.querySelector('p');
+const resetBtn = document.querySelector('button');
 const cells = ["", "", "", "", "", "", "", "", "",];
 let count = 0;
-
-text.innerHTML = 'Começa com "X"';
 
 function createCells() {
     cells.forEach(function (_cell, index) {
         const cellElement = document.createElement("div");
-        cellElement.classList.add('celula');
+        cellElement.classList.add('cell');
         cellElement.id = index;
-        tabuleiro.append(cellElement);
+        boardElement.append(cellElement);
         cellElement.addEventListener("click", addPlayer);
     });
-}
+};
 
 function addPlayer(e) {
     const cell = e.target;
@@ -31,32 +30,42 @@ function addPlayer(e) {
 };
 
 function checkWinner() {
-    const cells = document.querySelectorAll('.celula');
+    const cells = document.querySelectorAll('.cell');
     const winnerCombinations = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8], //horizontal
         [0, 3, 6], [1, 4, 7], [2, 5, 8], // vertical
         [0, 4, 8], [2, 4, 6] // diagonal
     ];
-    const hasText = Array.from(cells).every(function (cell) {
+    const isDraw = Array.from(cells).every(function (cell) {
         return cell.innerText !== '';
-
     });
 
     for (const condition of winnerCombinations) {
-
         let [a, b, c] = condition;
-        // console.log([a, b, c] = condition);
+
         if (cells[a].innerText && (cells[a].innerText == cells[b].innerText &&
             cells[a].innerText == cells[c].innerText)) {
-            console.log(cells[a].innerText);
+
             text.innerHTML = `GANHOU "${cells[a].innerText}"`;
-        }
+            cells.forEach(function (cell) {
+                cell.replaceWith(cell.cloneNode(true));
+            });
+        } else if (isDraw) {
+            text.innerHTML = "EMPATOU";
+        };;
+    };
+};
 
-        // text.innerHTML = "EMPATOU";
-    }
-    if (hasText) {
-        text.innerHTML = "EMPATOU";
-    }
-}
-
-createCells();
+resetBtn.addEventListener('click', function () {
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(function (cell) {
+        cell.innerHTML = "";
+        cell.addEventListener('click', addPlayer);
+    });
+    count = 0;
+    text.innerHTML = 'Começa com "X"';
+});
+window.addEventListener('DOMContentLoaded', function () {
+    text.innerHTML = 'Começa com "X"';
+    createCells();
+});
