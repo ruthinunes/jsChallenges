c6a520c44b152c04e3daa45c65ae729a343e0f5e
ruthinunes <ruthinunesdev@gmail.com>
2023-04-09 10:54:22 -0300

    Uploading js file

---
+++ tipCalculator/app.js
@@ -0,0 +1,30 @@
+const formElement = document.querySelector('form');
+const billElement = document.querySelector('#billAmount');
+const tipElement = document.querySelector('#tipPorcent');
+const resultElement = document.querySelector('.result');
+
+formElement.addEventListener('submit', function (e) {
+    e.preventDefault();
+    getValues();
+});
+
+function getValues() {
+    if (billElement.value == "" || tipElement.value == "") {
+        alert('Por favor insira todos os dados!');
+        return;
+    }
+    const billValue = parseFloat(billElement.value);
+    const tipValue = parseFloat(tipElement.value);
+   
+    calculate(billValue, tipValue);
+};
+
+function calculate(billValue, tipValue) {
+    const totalTip = (billValue * tipValue) / 100
+    const totalBill = billValue + totalTip;
+    const resultContent = `<p class="tip-result">Total em gorgeta: <span>R$ ${totalTip.toFixed(2)}</span></p>
+    <p class="bill-result">Total da conta: <span>R$ ${totalBill.toFixed(2)}</span></p>`;
+
+    resultElement.classList.add('active');
+    resultElement.innerHTML = resultContent;
+};
